b55fc6f95b61f55b10a6b9d2e26558af1982988a
dobefore <1432338032@qq.com>
2020-11-05 13:26:23 +0800

    modify_python_install_way

--- src/check_python.rs
+++ src/check_python.rs
@@ -14,18 +14,20 @@ pub mod check_python {
             let py_in = Command::new("python")
                 .args(&["check_python.py"])
                 .output()
-                .expect("Failed to install python");
-            let mut fk = String::new();
-            File::open("check_python.txt")?.read_to_string(&mut fk)?;
-            match fk.trim() {
-                "1" => {
-                    println!("确认python已安装");
-                    break Ok(());
-                }
+    .expect("Failed to start print py-version process");
+    let py_str=String::from_utf8(py_in_.stdout).unwrap();
+   
+   if py_str !=""{
+   let sp_py:Vec<&str>=py_str.trim().split(|c| c==' ' || c=='.').collect();
+let py_ver=format!("{}.{}",sp_py[1],sp_py[2]);
+match py_ver.as_str() {
+    "3.9" => println!(),
+    _ => {println!("检测到安装了3.9以外的python，请卸载重装3.9")},   
+}
                 _ => {
                     println!("未正确安装python或未正确识别py文件,请关闭本页面重新打开auto_install");
                     Duration::from_secs(1);
-                }
+}
             };
         }
     }
