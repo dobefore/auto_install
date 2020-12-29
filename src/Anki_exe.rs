7f8a9cd30a6ca982cf66cf4d6812553ae2efb4a0
dobefore <1432338032@qq.com>
2020-12-29 16:24:27 +0800

    add_ver_support_36_and_later

--- src/Anki_exe.rs
+++ src/Anki_exe.rs
@@ -1,14 +1,28 @@
 pub use Anki_exe::anki_send_desktop;
 #[allow(non_snake_case)]
 pub mod Anki_exe {
+    use std::fs;
     use std::process::Command;
-
+    
     pub fn anki_send_desktop() {
-        let _py_in_ = Command::new("python")
-            .arg(r"anki_server_v_2.1.26\anki-sync-server\shortcut_server.py")
-            .spawn()
-            .expect("Failed to start change PC anki IP process")
-            .wait_with_output();
+       // send anki_server.exe to desktop using py scripts
+
+        //read addon choice num from txt
+        let addon_num = fs::read_to_string(r"pre_install\addon_choice_num.txt")
+            .unwrap()
+            .trim()
+            .parse::<u8>()
+            .unwrap();
+
+        let _py_in_ = if addon_num == 3 {
+            Command::new(r"anki_server_v_2.1.36\shortcut_server.exe")
+                .status()
+                .expect("Failed to send anki-server to desktop")
+        } else {
+            Command::new(r"anki_server_v_2.1.26\anki-sync-server\shortcut_server.exe")
+                .status()
+                .expect("Failed to send anki-server to desktop")
+        };
         println!("已将anki服务器软件发送到桌面快捷方式");
         println!("在桌面找到刚发送的anki_server，双击打开,点击按钮 添加账号，如教程图所示");
         println!("点击按钮 打开服务器，即可打开同步服务，如教程图所示即表示正常");
---
+++ src/check_pc_anki_ver.rs
@@ -0,0 +1,37 @@
+pub use check_pc_anki_ver::{output_pc_anki_ver, PcAnkiVer};
+//types: 2.1.36 and later 、 2.1.26 and before
+pub mod check_pc_anki_ver {
+    use std::fs::File;
+    use std::io::{BufReader, Read};
+    use std::path::Path;
+    use std::process::Command;
+    pub enum PcAnkiVer {
+        Ver22,
+        Ver36,//36 and later
+        Ver26,
+        VerErr, //28-35
+    }
+
+    pub fn output_pc_anki_ver<S: AsRef<Path>>(pc_anki_ver_path: S) -> PcAnkiVer {
+        //get ver_num(ie,36) from txt
+        let ver_value = {
+            let _cmd = Command::new(r"pre_install\get_pc_anki_ver.exe")
+                .status()
+                .expect("error");
+            let mut ver_ = String::new();
+            let f = File::open(pc_anki_ver_path).unwrap();
+            let mut reader = BufReader::new(f);
+            reader.read_to_string(&mut ver_).unwrap();
+            ver_
+        };
+
+        let ver = ver_value.trim().parse::<u32>();
+        match ver {
+            Ok(_x @ 10..=22) => return PcAnkiVer::Ver22,
+            Ok(_x @ 23..=26) => return PcAnkiVer::Ver26,
+            Ok(_x ) if _x>=36=> return PcAnkiVer::Ver36,
+            //28-35
+            _ => return PcAnkiVer::VerErr,
+        }
+    }
+}
